d878d4c1bc325084d60f479944566ca24fbb410d
Irfan Ul Haq <Arfanulhaq1991@gmail.com>
2022-08-18 15:21:17 +0500

    FetchCitiesRepository: test case refactored to accomodate the changes

--- app/src/test/java/com/example/citysearch/CitiesRepositoryShould.kt
+++ app/src/test/java/com/example/citysearch/CitiesRepositoryShould.kt
@@ -11,41 +11,45 @@ import java.io.IOException
 class CitiesRepositoryShould : BaseTest(){
    // At the moment, No idea of the complete logic and design of the citiesRepository
     @MockK
-   lateinit var remoteDataSource : RemoteDataSource
+   private lateinit var remoteDataSource : RemoteDataSource
+   private lateinit var citiesRepository : CitiesRepository
 
    @Before
     override fun setUp() {
         super.setUp()
+       citiesRepository = CitiesRepository(remoteDataSource)
     }
     @Test
     fun returnNoCity(){
         every { remoteDataSource.fetchCities() } answers { emptyList()}
-        val citiesRepository = CitiesRepository(remoteDataSource)
-        Truth.assertThat(citiesRepository.fetchCities()).isEqualTo(emptyList<String>())
+        val expected = Result.success(emptyList<String>())
+        val result =  citiesRepository.fetchCities()
+        Truth.assertThat(result).isEqualTo(expected)
     }
 
     @Test
     fun returnOneCity(){
         every { remoteDataSource.fetchCities() } answers { listOf("London") }
-        val citiesRepository = CitiesRepository(remoteDataSource)
-        Truth.assertThat(citiesRepository.fetchCities()).isEqualTo(listOf("London"))
+        val expected = Result.success(listOf("London"))
+        val result =  citiesRepository.fetchCities()
+        Truth.assertThat(result).isEqualTo(expected)
     }
 
 
     @Test
     fun returnManyCities(){
         every { remoteDataSource.fetchCities() } answers { listOf("London","Yorkshire") }
-        val citiesRepository = CitiesRepository(remoteDataSource)
-        Truth.assertThat(citiesRepository.fetchCities()).isEqualTo(listOf("London","Yorkshire"))
+        val expected = Result.success(listOf("London","Yorkshire"))
+        val result =  citiesRepository.fetchCities()
+        Truth.assertThat(result).isEqualTo(expected)
     }
-
-
 
     @Test
     fun returnError(){
         every { remoteDataSource.fetchCities() } throws IOException("No internet")
         val citiesRepository = CitiesRepository(remoteDataSource)
-        Truth.assertThat(isFailureWithMessage(citiesRepository.fetchCities(),"No internet")).isTrue()
+        val result =  citiesRepository.fetchCities()
+        Truth.assertThat(isFailureWithMessage(result,"No internet")).isTrue()
     }
 
 
