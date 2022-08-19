0c3cfb93cd8f0a4f01f469366f06b7b1b539c5dd
Irfan Ul Haq <Arfanulhaq1991@gmail.com>
2022-08-19 23:43:22 +0500

    FetchCitiesUseCase: with the help of the use unit test use case modified to apply bussiness rule of sorting and acceptance is passed

--- app/src/main/java/com/example/citysearch/domain/FetchCitiesUseCase.kt
+++ app/src/main/java/com/example/citysearch/domain/FetchCitiesUseCase.kt
@@ -2,11 +2,20 @@ package com.example.citysearch.domain
 
 import com.example.citysearch.City
 import com.example.citysearch.data.CitiesRepository
+import java.text.Collator
+import java.util.*
 
 class FetchCitiesUseCase(private val citiesRepository: CitiesRepository) {
 
-    // No clear idea at the moment e.g like item per page
     suspend operator fun invoke ():Result<List<City>> {
-       return citiesRepository.fetchCities()
+       return citiesRepository.fetchCities().map { sortingRul(it) }
      }
+
+    // Applying Business rule
+ private fun sortingRul(cities:List<City>):List<City>{
+        val usCollator: Collator = Collator.getInstance(Locale.UK)
+        usCollator.strength = Collator.PRIMARY
+        return cities.sortedWith(compareBy(usCollator) { it.cityName})
+    }
 }
+
--- app/src/test/java/com/example/citysearch/domain/FetchCitiesUseCaseShould.kt
+++ app/src/test/java/com/example/citysearch/domain/FetchCitiesUseCaseShould.kt
@@ -1,8 +1,12 @@
 package com.example.citysearch.domain
 
 import com.example.citysearch.BaseTest
+import com.example.citysearch.TestDataProvider
 import com.example.citysearch.data.CitiesRepository
+import com.google.common.truth.Truth
+import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.verifyAll
 import kotlinx.coroutines.test.runTest
@@ -13,7 +17,8 @@ class FetchCitiesUseCaseShould : BaseTest() {
 
 
     lateinit var fetchCitiesUseCase: FetchCitiesUseCase
-    @RelaxedMockK
+
+    @MockK
     lateinit var citiesRepository: CitiesRepository
 
     @Before
@@ -22,11 +27,29 @@ class FetchCitiesUseCaseShould : BaseTest() {
         fetchCitiesUseCase = FetchCitiesUseCase(citiesRepository)
     }
 
+
+
+    // White box test to verify the behavior
     @Test
     fun fetchCities() = runTest {
+        coEvery { citiesRepository.fetchCities() } answers { Result.success(TestDataProvider.provideDomainModels()) }
         fetchCitiesUseCase()
         coVerify { citiesRepository.fetchCities() }
+    }
 
+
+    // black box test to verify the state
+    @Test
+    fun sortTheCities() = runTest {
+
+        coEvery { citiesRepository.fetchCities() } answers { Result.success(TestDataProvider.provideDomainModels()) }
+        val expected = Result.success(TestDataProvider.sort(TestDataProvider.provideDomainModels()))
+
+        val result = fetchCitiesUseCase()
+
+        coVerify { citiesRepository.fetchCities() }
+        Truth.assertThat(result)
+            .isEqualTo(expected)
     }
 
 }
