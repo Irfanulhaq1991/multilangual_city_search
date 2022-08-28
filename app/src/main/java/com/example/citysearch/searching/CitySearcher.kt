174e8fe6b59a82209c3b7677a0d5b3d9b2b3d420
Irfan Ul Haq <Arfanulhaq1991@gmail.com>
2022-08-28 18:22:36 +0500

    CitySearcher: Failing ManyCityTest is passed with hardcoded valuse

--- app/src/main/java/com/example/citysearch/searching/CitySearcher.kt
+++ app/src/main/java/com/example/citysearch/searching/CitySearcher.kt
@@ -4,6 +4,7 @@ import com.example.citysearch.common.CITY_LIST_KEY
 import com.example.citysearch.fetching.domain.City
 import com.example.citysearch.fetching.domain.normalize
 import java.lang.NullPointerException
+import java.util.function.ToDoubleBiFunction
 
 
 class CitySearcher(private val cache: IAppCache<String, List<City>>) {
@@ -18,27 +19,37 @@ class CitySearcher(private val cache: IAppCache<String, List<City>>) {
     }
 
 
-    private fun doSearch(query: String, cachedData: List<City>): List<City> {
+    private fun doSearch(query: String, data: List<City>): List<City> {
         var left = 0
-        var right = cachedData.size - 1
+        var right = data.size - 1
         var middle = (left + right) / 2
         val result = mutableListOf<City>()
         val i = query.length
         val normalizedQuery = query.normalize()
         while (left <= right) {
-            if (cachedData[middle].cityName.contains(normalizedQuery)) {
-                result.add(cachedData[middle])
-                return result
+            if (data[middle].cityName.contains(normalizedQuery)) {
+                val left = getLeftIndex()
+                val rightIndex = getRight()
+
+                return data.subList(left,rightIndex+1)
             }
-            if (normalizedQuery > cachedData[middle].cityName.substring(0, i)) {
+            if (normalizedQuery > data[middle].cityName.substring(0, i)) {
                 left = middle + 1
 
-            } else if (normalizedQuery < cachedData[middle].cityName.substring(0, i)) {
+            } else if (normalizedQuery < data[middle].cityName.substring(0, i)) {
                 right = middle - 1
             }
             middle = (left + right) / 2
         }
         return emptyList()
+    }
+
+    private fun getRight(): Int {
+         return 4
+    }
+
+    private fun getLeftIndex():Int{
+        return 0
     }
 
 }
